ecad9badc1386678ade212b28a0516d51ef311ee
krishna-2017 <krishna_2017@hotmail.com>
2025-04-22 20:32:46 -0400

    Set up ProductProperties class

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Script loaded successfully!");
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
