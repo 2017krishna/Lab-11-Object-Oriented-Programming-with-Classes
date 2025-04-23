e03530ead017a608b88ee5de551594ca0ce7b2e4
krishna-2017 <krishna_2017@hotmail.com>
2025-04-22 20:35:56 -0400

    Created PerishableProductProperties subclass with expirationDate property

--- script.js
+++ script.js
@@ -14,9 +14,20 @@ class ProductProperties {
     }
 }
 
+class PerishableProductProperties extends ProductProperties {
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    toString() {
+        return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
+    }
+}
+
 // Testing the ProductProperties class
-const product1 = new ProductProperties("Apple", 0.5, 100);
-const product2 = new ProductProperties("Banana", 0.3, 150);
+const product1 = new ProductProperties("Milk", 0.5, 100);
+const product2 = new ProductProperties("Cereal", 0.3, 150);
 
 console.log(product1.toString());
 console.log(`Total Value: $${product1.getTotalValue().toFixed(2)}`);
