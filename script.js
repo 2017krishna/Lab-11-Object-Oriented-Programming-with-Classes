e1946f81848529eea43a341cb7e941ee549ffb86
krishna-2017 <krishna_2017@hotmail.com>
2025-04-22 20:38:32 -0400

    Updated PerishableProductProperties samples with new products

--- script.js
+++ script.js
@@ -34,3 +34,13 @@ console.log(`Total Value: $${product1.getTotalValue().toFixed(2)}`);
 
 console.log(product2.toString());
 console.log(`Total Value: $${product2.getTotalValue().toFixed(2)}`);
+
+// Testing the PerishableProductProperties class
+const perishable1 = new PerishableProductProperties("Strawberries", 3.5, 20, "2025-04-30");
+const perishable2 = new PerishableProductProperties("Lettuce", 1.0, 40, "2025-04-25");
+
+console.log(perishable1.toString());
+console.log(`Total Value: $${perishable1.getTotalValue().toFixed(2)}`);
+
+console.log(perishable2.toString());
+console.log(`Total Value: $${perishable2.getTotalValue().toFixed(2)}`);
